1b9d9185a3b729c5f3a05992cf7c6357f2937a57
Praveen <ypraveen760@gmail.com>
2024-10-08 22:07:52 +0530

    Episod 01

---
+++ apps.js
@@ -0,0 +1,8 @@
+const parent=React.createElement("div",{id:"parent"},
+   [ React.createElement("div",{id:"child1"},
+    [React.createElement("h1",{},"Im h1 tag"),React.createElement("h2",{},"Im h2 tag")]), React.createElement("div",{id:"child2"},
+        [React.createElement("h1",{},"Im h1 tag"),React.createElement("h2",{},"Im h2 tag")])]
+);
+const root =ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent)
+console.log(parent) 
