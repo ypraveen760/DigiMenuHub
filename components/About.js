2dd6c2e1bb0ebbb4c8b3f131fdc7f52c3d9788c0
Praveen Yadav <ypraveen760@Praveens-Mac-mini.local>
2024-10-20 14:20:39 +0530

    Added Shimmer UI and changed about from fetch to direct due to to many fetch request error

--- components/About.js
+++ components/About.js
@@ -3,47 +3,48 @@ import Shimmer from "./Shimmer";
 import UserContext from "../utils/UserContext";
 import { GITHUB_API, CORS_PROXY } from "../utils/constans";
 const About = () => {
-  const [userData, setUserData] = useState("");
-  const { loggedInUser } = useContext(UserContext);
+  // const [userData, setUserData] = useState("");
+  // const { loggedInUser } = useContext(UserContext);
 
-  useEffect(() => {
-    getData();
-  }, []);
-  const getData = async () => {
-    try {
-      data = await fetch(CORS_PROXY + GITHUB_API);
-      user = await data.json();
-      setUserData(user);
-      console.log(user);
-    } catch (error) {
-      console.log("ERROR FETCHING DATA", error);
-    }
-  };
-  if (userData.length === 0) {
-    return <Shimmer />;
-  }
-  const { name, avatar_url, bio, login, email } = userData || {};
+  // useEffect(() => {
+  //   getData();
+  // }, []);
+  // const getData = async () => {
+  //   try {
+  //     data = await fetch(CORS_PROXY + GITHUB_API);
+  //     user = await data.json();
+  //     setUserData(user);
+  //     console.log(user);
+  //   } catch (error) {
+  //     console.log("ERROR FETCHING DATA", error);
+  //   }
+  // };
+  // if (userData.length === 0) {
+  //   return <Shimmer />;
+  // }
+  // const { name, avatar_url, bio, login, email } = userData || {};
   return (
     <div className="max-w-80 p-5 m-auto bg-orange-50 *:my-2 ">
-      <h1 className="text-lg font-semibold ">User:{loggedInUser}</h1>
       <img
         className="rounded-full w-72 border border-orange-400 my-3"
-        src={avatar_url}
+        src="https://avatars.githubusercontent.com/u/32950689?v=4"
       ></img>
       <h2 className="text-lg font-semibold">
-        Name: <span className="font-light">{name}</span>
+        Name: <span className="font-light">Praveen Yadav</span>
       </h2>
       <h3 className="text-lg font-semibold">
-        Username: <span className="font-light">{login}</span>
+        Username: <span className="font-light">ypraveen760</span>
       </h3>
       <h4>
         <span className="text-lg font-semibold">Email: </span>
-        {email || "Not Given"}
+        ypraveen760@gmail.com
       </h4>
       <p>
         {" "}
         <span className="text-lg font-semibold">Bio: </span>
-        {bio || "Not Given"}
+        Hi, I’m Praveen Yadav! I’m a front-end developer skilled in JavaScript,
+        React, HTML, CSS, Tailwind CSS, Bootstrap, Material UI, React Router,
+        and REST APIs.
       </p>
     </div>
   );
--- components/Shimmer.js
+++ components/Shimmer.js
@@ -1,9 +1,16 @@
 const Shimmer = () => {
   return (
     <div className="flex m-5 flex-wrap">
-      <div className="max-w-60 border  bg-gray-300 min-h-[500px] min-w-60 border-gray-300 m-2 p-2 *:text-lg  hover:drop-shadow-xl rounded-md">
-        <div className="mb-4 min-h-[200px] bg-slate-300 rounded-md"></div>
-      </div>
+      <div className="max-w-60 border  bg-gray-100 min-h-[500px] min-w-60 border-gray-100 m-2 p-2 *:text-lg  hover:drop-shadow-xl rounded-md"></div>
+      <div className="max-w-60 border  bg-gray-100 min-h-[500px] min-w-60 border-gray-100 m-2 p-2 *:text-lg  hover:drop-shadow-xl rounded-md"></div>
+      <div className="max-w-60 border  bg-gray-100 min-h-[500px] min-w-60 border-gray-100 m-2 p-2 *:text-lg  hover:drop-shadow-xl rounded-md"></div>
+      <div className="max-w-60 border  bg-gray-100 min-h-[500px] min-w-60 border-gray-100 m-2 p-2 *:text-lg  hover:drop-shadow-xl rounded-md"></div>
+      <div className="max-w-60 border  bg-gray-100 min-h-[500px] min-w-60 border-gray-100 m-2 p-2 *:text-lg  hover:drop-shadow-xl rounded-md"></div>
+      <div className="max-w-60 border  bg-gray-100 min-h-[500px] min-w-60 border-gray-100 m-2 p-2 *:text-lg  hover:drop-shadow-xl rounded-md"></div>
+      <div className="max-w-60 border  bg-gray-100 min-h-[500px] min-w-60 border-gray-100 m-2 p-2 *:text-lg  hover:drop-shadow-xl rounded-md"></div>
+      <div className="max-w-60 border  bg-gray-100 min-h-[500px] min-w-60 border-gray-100 m-2 p-2 *:text-lg  hover:drop-shadow-xl rounded-md"></div>
+      <div className="max-w-60 border  bg-gray-100 min-h-[500px] min-w-60 border-gray-100 m-2 p-2 *:text-lg  hover:drop-shadow-xl rounded-md"></div>
+      <div className="max-w-60 border  bg-gray-100 min-h-[500px] min-w-60 border-gray-100 m-2 p-2 *:text-lg  hover:drop-shadow-xl rounded-md"></div>
     </div>
   );
 };
