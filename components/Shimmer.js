5bfd613869281019f5221501868f2af96024c5f9
Praveen Yadav <ypraveen760@Praveens-Mac-mini.local>
2024-10-20 14:09:09 +0530

    Added Shimmer ui

--- components/Shimmer.js
+++ components/Shimmer.js
@@ -1,32 +1,8 @@
 const Shimmer = () => {
   return (
-    <div className="flex flex-wrap">
-      <div className="max-w-60 border bg-gray-50-50 min-h-[500px]  border-black m-2 p-2 *:text-lg  hover:drop-shadow-xl rounded-md">
-        <div className="mb-4 min-h-[200px] bg-slate-50 rounded-md"></div>
-      </div>
-      <div className="max-w-60 border bg-gray-50-50 min-h-[500px]  border-black m-2 p-2 *:text-lg  hover:drop-shadow-xl rounded-md">
-        <div className="mb-4 min-h-[200px] bg-slate-50 rounded-md"></div>
-      </div>
-      <div className="max-w-60 border bg-gray-50-50 min-h-[500px]  border-black m-2 p-2 *:text-lg  hover:drop-shadow-xl rounded-md">
-        <div className="mb-4 min-h-[200px] bg-slate-50 rounded-md"></div>
-      </div>
-      <div className="max-w-60 border bg-gray-50-50 min-h-[500px]  border-black m-2 p-2 *:text-lg  hover:drop-shadow-xl rounded-md">
-        <div className="mb-4 min-h-[200px] bg-slate-50 rounded-md"></div>
-      </div>
-      <div className="max-w-60 border bg-gray-50-50 min-h-[500px]  border-black m-2 p-2 *:text-lg  hover:drop-shadow-xl rounded-md">
-        <div className="mb-4 min-h-[200px] bg-slate-50 rounded-md"></div>
-      </div>
-      <div className="max-w-60 border bg-gray-50-50 min-h-[500px]  border-black m-2 p-2 *:text-lg  hover:drop-shadow-xl rounded-md">
-        <div className="mb-4 min-h-[200px] bg-slate-50 rounded-md"></div>
-      </div>
-      <div className="max-w-60 border bg-gray-50-50 min-h-[500px]  border-black m-2 p-2 *:text-lg  hover:drop-shadow-xl rounded-md">
-        <div className="mb-4 min-h-[200px] bg-slate-50 rounded-md"></div>
-      </div>
-      <div className="max-w-60 border bg-gray-50-50 min-h-[500px]  border-black m-2 p-2 *:text-lg  hover:drop-shadow-xl rounded-md">
-        <div className="mb-4 min-h-[200px] bg-slate-50 rounded-md"></div>
-      </div>
-      <div className="max-w-60 border bg-gray-50-50 min-h-[500px]  border-black m-2 p-2 *:text-lg  hover:drop-shadow-xl rounded-md">
-        <div className="mb-4 min-h-[200px] bg-slate-50 rounded-md"></div>
+    <div className="flex m-5 flex-wrap">
+      <div className="max-w-60 border  bg-gray-300 min-h-[500px] min-w-60 border-gray-300 m-2 p-2 *:text-lg  hover:drop-shadow-xl rounded-md">
+        <div className="mb-4 min-h-[200px] bg-slate-300 rounded-md"></div>
       </div>
     </div>
   );
