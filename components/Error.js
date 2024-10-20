02e23c2094f3ca92d7b660bb4ed93caa59138a01
Praveen Yadav <ypraveen760@Praveens-Mac-mini.local>
2024-10-20 14:38:25 +0530

    updated error page message

--- components/Error.js
+++ components/Error.js
@@ -7,6 +7,10 @@ const Error = () => {
   return (
     <div>
       <main class="h-screen w-full flex flex-col justify-center items-center bg-[#1A2238]">
+        <div class="bg-[#FF6A3D] px-2 text-sm rounded p-2 ">
+          If you are using phone homepage will not work due to CROS error ,try
+          to open in pc.
+        </div>
         <h1 class="text-9xl font-extrabold text-white tracking-widest">404</h1>
         <div class="bg-[#FF6A3D] px-2 text-sm rounded p-2 ">
           Page Not Found OR ERROR WITH SWIGGY API, PLEASE TRY LATER.
